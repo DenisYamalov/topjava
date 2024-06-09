6b2acf481205a607bae5cecdec2f259a8245e3d6
DenisYamalov <nokiden91@gmail.com>
2024-06-09 14:16:45 +0300

    fix: returned mapping in web.xml

--- src/main/java/ru/javawebinar/topjava/web/MealServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -5,17 +5,14 @@ import ru.javawebinar.topjava.model.MealTo;
 import ru.javawebinar.topjava.util.MealsUtil;
 
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import java.io.IOException;
 import java.util.List;
 
 import static org.slf4j.LoggerFactory.getLogger;
 
-@WebServlet("/meals")
 public class MealServlet extends HttpServlet {
     private static final Logger log = getLogger(MealServlet.class);
 
