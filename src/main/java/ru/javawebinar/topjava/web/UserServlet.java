cec0431cfc1dbfa3b9f699aea764e05289afd00d
DenisYamalov <nokiden91@gmail.com>
2024-06-17 16:23:08 +0300

    feat: add user select

--- src/main/java/ru/javawebinar/topjava/web/UserServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/UserServlet.java
@@ -16,6 +16,9 @@ public class UserServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         log.debug("forward to users");
-        request.getRequestDispatcher("/users.jsp").forward(request, response);
+        int userId = Integer.parseInt(request.getParameter("userId"));
+        SecurityUtil.setUserId(userId);
+        log.info("User id = {}", userId);
+        response.sendRedirect("meals");
     }
 }
