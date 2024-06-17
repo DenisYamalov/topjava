7c8899a2003bf436b748852ae98bfc1c9f4c09df
DenisYamalov <nokiden91@gmail.com>
2024-06-17 22:36:49 +0300

    fix: dateTime comparison

--- src/main/java/ru/javawebinar/topjava/util/DateTimeUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/DateTimeUtil.java
@@ -1,19 +1,17 @@
 package ru.javawebinar.topjava.util;
 
 import java.time.LocalDateTime;
-import java.time.LocalTime;
-import java.time.chrono.ChronoLocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class DateTimeUtil {
     private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+//
+//    public static boolean isBetweenHalfOpen(LocalTime lt, LocalTime startTime, LocalTime endTime) {
+//        return !lt.isBefore(startTime) && lt.isBefore(endTime);
+//    }
 
-    public static boolean isBetweenHalfOpen(LocalTime lt, LocalTime startTime, LocalTime endTime) {
-        return !lt.isBefore(startTime) && lt.isBefore(endTime);
-    }
-
-    public static <T extends ChronoLocalDate> boolean isBetweenHalfOpen(T check, T start, T end) {
-        return check.compareTo(start) >= 0 && check.compareTo(end) <= 0;
+    public static <T extends Comparable> boolean isBetweenHalfOpen(T check, T start, T end) {
+        return check.compareTo(start) >= 0 && check.compareTo(end) < 0;
     }
 
     public static String toString(LocalDateTime ldt) {
