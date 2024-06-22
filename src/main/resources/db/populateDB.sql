06add24857f74c4e1f81b71261096acc248896b3
DenisYamalov <nokiden91@gmail.com>
2024-06-22 14:40:41 +0300

    feat: add and  populate sql scripts for meals

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,5 +1,9 @@
-DELETE FROM user_role;
-DELETE FROM users;
+DELETE
+FROM user_role;
+DELETE
+FROM users;
+DELETE
+FROM meals;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
 
 INSERT INTO users (name, email, password)
@@ -10,3 +14,19 @@ VALUES ('User', 'user@yandex.ru', 'password'),
 INSERT INTO user_role (role, user_id)
 VALUES ('USER', 100000),
        ('ADMIN', 100001);
+
+INSERT INTO meals (user_id, dateTime, description, calories)
+VALUES (100000, '2020-10-30 10:00', 'Завтрак', 500),
+       (100000, '2020-10-30 13:00', 'Обед', 1000),
+       (100000, '2020-10-30 20:00', 'Ужин', 500),
+       (100000, '2020-10-31 00:00', 'Еда на граничное значение', 100),
+       (100000, '2020-10-31 10:00', 'Завтрак', 1000),
+       (100000, '2020-10-31 13:00', 'Обед', 500),
+       (100000, '2020-10-31 20:00', 'Ужин', 410),
+       (100001, '2020-10-30 10:00', 'Завтрак ADMIN', 500),
+       (100001, '2020-10-30 13:00', 'Обед ADMIN', 1000),
+       (100001, '2020-10-30 20:00', 'Ужин ADMIN', 500),
+       (100001, '2020-10-31 00:00', 'Еда на граничное значение ADMIN', 100),
+       (100001, '2020-10-31 10:00', 'Завтрак ADMIN', 1000),
+       (100001, '2020-10-31 13:00', 'Обед ADMIN', 500),
+       (100001, '2020-10-31 20:00', 'Ужин ADMIN', 410);
