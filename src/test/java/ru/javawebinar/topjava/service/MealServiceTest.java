a7c9d8674cb61e02f237e4b75d1fcee15edb15bb
DenisYamalov <nokiden91@gmail.com>
2024-06-24 11:52:47 +0300

    feat: add tests for foreign food access and duplicate tests

--- src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
@@ -4,24 +4,24 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.bridge.SLF4JBridgeHandler;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.SqlConfig;
 import org.springframework.test.context.junit4.SpringRunner;
 import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.util.exception.NotFoundException;
 
 import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.List;
 
+import static org.junit.Assert.assertThrows;
 import static ru.javawebinar.topjava.MealTestData.*;
 import static ru.javawebinar.topjava.UserTestData.admin;
 import static ru.javawebinar.topjava.UserTestData.user;
 
-@ContextConfiguration({
-        "classpath:spring/spring-app.xml",
-        "classpath:spring/spring-db.xml"
-})
+@ContextConfiguration({"classpath:spring/spring-app.xml", "classpath:spring/spring-db.xml"})
 @RunWith(SpringRunner.class)
 @Sql(scripts = "classpath:db/populateDB.sql", config = @SqlConfig(encoding = "UTF-8"))
 public class MealServiceTest {
@@ -41,16 +41,26 @@ public class MealServiceTest {
     }
 
     @Test
+    public void getNotFound() {
+        assertThrows(NotFoundException.class, () -> service.get(userMeal1.getId(), admin.getId()));
+    }
+
+    @Test
     public void delete() {
         service.delete(userMeal1.getId(), user.getId());
+    }
+
+    @Test
+    public void deleteNotFound() {
+        assertThrows(NotFoundException.class, () -> service.delete(userMeal1.getId(), admin.getId()));
     }
 
     @Test
     public void getBetweenInclusive() {
         //Meals list of 2020-01-30
         List<Meal> mealsBetweenDate = Arrays.asList(userMeal3, userMeal2, userMeal1);
-        assertMatch(service.getBetweenInclusive(LocalDate.of(2020, 1, 30), LocalDate.of(2020, 1, 30), user.getId()),
-                    mealsBetweenDate);
+        assertMatch(service.getBetweenInclusive(LocalDate.of(2020, 1, 30),
+                LocalDate.of(2020, 1, 30), user.getId()), mealsBetweenDate);
     }
 
     @Test
@@ -69,6 +79,11 @@ public class MealServiceTest {
     }
 
     @Test
+    public void updateNotFound() {
+        assertThrows(NotFoundException.class, () -> service.update(userMeal1, admin.getId()));
+    }
+
+    @Test
     public void create() {
         Meal created = service.create(getNewMeal(), user.getId());
         Integer createdId = created.getId();
@@ -76,5 +91,16 @@ public class MealServiceTest {
         newMeal.setId(createdId);
         assertMatch(created, newMeal);
         assertMatch(service.get(createdId, user.getId()), newMeal);
+    }
+
+    @Test
+    public void duplicateMailCreate() {
+        assertThrows(DataAccessException.class, () -> service.create(userMeal1, user.getId()));
+    }
+
+    @Test
+    public void duplicateDateTimeCreate() {
+        Meal duplcateDateMeal = new Meal(userMeal1.getDateTime(), "Duplicate date-time meal", 100);
+        assertThrows(DataAccessException.class, () -> service.create(duplcateDateMeal, user.getId()));
     }
 }
