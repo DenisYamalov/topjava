f276069d2c346901991cc58df70d2bb9bcac40b3
DenisYamalov <nokiden91@gmail.com>
2024-07-16 14:52:07 +0300

    refactor: move duplicated code to AbstractServiceTest

--- src/test/java/ru/javawebinar/topjava/service/AbstractUserServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/AbstractUserServiceTest.java
@@ -1,5 +1,6 @@
 package ru.javawebinar.topjava.service;
 
+import org.junit.Assume;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
@@ -80,6 +81,7 @@ public abstract class AbstractUserServiceTest extends AbstractServiceTest {
 
     @Test
     public void createWithException() throws Exception {
+        Assume.assumeFalse(isJdbcProfile());
         validateRootCause(ConstraintViolationException.class, () -> service.create(new User(null, "  ", "mail@yandex.ru", "password", Role.USER)));
         validateRootCause(ConstraintViolationException.class, () -> service.create(new User(null, "User", "  ", "password", Role.USER)));
         validateRootCause(ConstraintViolationException.class, () -> service.create(new User(null, "User", "mail@yandex.ru", "  ", Role.USER)));
--- src/test/java/ru/javawebinar/topjava/service/jdbc/JdbcUserServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/jdbc/JdbcUserServiceTest.java
@@ -1,30 +1,10 @@
 package ru.javawebinar.topjava.service.jdbc;
 
-import org.junit.Assume;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.env.Environment;
 import org.springframework.test.context.ActiveProfiles;
 import ru.javawebinar.topjava.service.AbstractUserServiceTest;
-
-import java.util.Arrays;
 
 import static ru.javawebinar.topjava.Profiles.JDBC;
 
 @ActiveProfiles(JDBC)
 public class JdbcUserServiceTest extends AbstractUserServiceTest {
-
-    @Autowired
-    Environment env;
-
-    private boolean isJdbcProfile() {
-        String[] activeProfiles = env.getActiveProfiles();
-        Arrays.stream(activeProfiles).forEach(System.out::println);
-        return Arrays.stream(activeProfiles).anyMatch(s -> s.contains(JDBC.toLowerCase()));
-    }
-
-    @Override
-    public void createWithException() throws Exception {
-        Assume.assumeFalse(isJdbcProfile());
-        super.createWithException();
-    }
 }
