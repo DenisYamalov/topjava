c0e09a4df433da1da5e69a1f5303a3b45024e1ee
DenisYamalov <nokiden91@gmail.com>
2024-07-18 00:03:07 +0300

    feat: add meal localization

--- src/main/java/ru/javawebinar/topjava/web/meal/JspMealController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/JspMealController.java
@@ -58,6 +58,7 @@ public class JspMealController extends AbstractMealController {
     @GetMapping("/create")
     public String create(Model model) {
         model.addAttribute("meal", new Meal(LocalDateTime.now().truncatedTo(ChronoUnit.MINUTES), "", 1000));
+        model.addAttribute("action", "create");
         return "mealForm";
     }
 
