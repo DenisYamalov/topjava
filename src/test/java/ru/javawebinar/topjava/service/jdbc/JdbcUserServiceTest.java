c77f87b004b83ddd01ed16f8136ee15506225bf0
DenisYamalov <nokiden91@gmail.com>
2024-07-23 21:53:09 +0300

    fix: add user without roles hsqldb

--- src/test/java/ru/javawebinar/topjava/service/jdbc/JdbcUserServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/jdbc/JdbcUserServiceTest.java
@@ -1,10 +1,17 @@
 package ru.javawebinar.topjava.service.jdbc;
 
+import org.junit.Test;
 import org.springframework.test.context.ActiveProfiles;
+import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.service.AbstractUserServiceTest;
 
 import static ru.javawebinar.topjava.Profiles.JDBC;
 
 @ActiveProfiles(JDBC)
 public class JdbcUserServiceTest extends AbstractUserServiceTest {
+
+    @Test
+    public void saveWithoutRoles(){
+        service.create(new User(null,"Guest2", "guest2@gmail.com", "guest2"));
+    }
 }
