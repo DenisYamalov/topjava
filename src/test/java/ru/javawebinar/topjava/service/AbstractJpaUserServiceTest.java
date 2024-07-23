902a4827ee2f2e8d5da0b881e1575c9f85bfbd52
DenisYamalov <nokiden91@gmail.com>
2024-07-23 11:15:28 +0300

    feat: replace user by admin at tests

--- src/test/java/ru/javawebinar/topjava/service/AbstractJpaUserServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/AbstractJpaUserServiceTest.java
@@ -1,7 +1,16 @@
 package ru.javawebinar.topjava.service;
 
+import org.junit.Before;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.CacheManager;
 import org.springframework.test.context.ContextConfiguration;
 
 @ContextConfiguration("classpath:spring/testConfig.xml")
 public abstract class AbstractJpaUserServiceTest extends AbstractUserServiceTest {
+    @Autowired
+    private CacheManager cacheManager;
+    @Before
+    public void setup() {
+        cacheManager.getCache("users").clear();
+    }
 }
